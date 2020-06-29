961e275143f68145a591197fe165e0955f8d3628
github username <github email address>
2020-06-29 15:09:55 -0600

    Create all required functionality for page.

--- app.js
+++ app.js
@@ -1,3 +1,62 @@
-let imageID = // pick a number between 1 and 3.
+let imageID = 1.
 let imageURL = `http://localhost:3000/movies/${imageID}`
+let upvotesURL = `http://localhost:3000/upvotes`
+let reviewsURL = 'http://localhost:3000/reviews'
 
+
+const $upvoteButton = document.querySelector('#upvote')
+const $movieName = document.querySelector('#movie-name');
+const $movieImage = document.querySelector('#movie-image');
+const $movieUpvotes = document.querySelector('#movie-upvotes');
+const $movieReviews = document.querySelector('#movie-reviews');
+const $form = document.querySelector('form');
+
+$upvoteButton.addEventListener('click', addUpvote);
+$form.addEventListener('submit', submitReview)
+
+fetch(imageURL)
+  .then(renderJson)
+  .then(renderPage)
+
+function renderJson(response) {
+  return response.json()
+}
+
+function postFetch(url, body) {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  })
+}
+
+function renderPage(movieData) {
+  $movieName.textContent = movieData.name;
+  $movieImage.src = movieData.image_url;
+  $movieUpvotes.textContent = movieData.upvotes_count;
+  movieData.reviews.forEach(review => {
+    createReviewLi(review.content)
+  })
+}
+
+function addUpvote(event) {
+  $movieUpvotes.textContent = +$movieUpvotes.textContent + 1;
+  postFetch(upvotesURL, { upvote: { movie_id: imageID } })
+}
+
+function submitReview(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const content = formData.get('content')
+  createReviewLi(content)
+  postFetch(reviewsURL, { review: { content } })
+  event.target.reset();
+}
+
+function createReviewLi(review) {
+  const $li = document.createElement('li');
+  $li.textContent = review;
+  $movieReviews.append($li);
+}
